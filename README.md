edce29bcde263ad87e01c1b1890d19b5ab5287b1
Juan Manuel Crescente <juan.crescente@tiendanube.com>
2025-03-27 15:48:00 -0300

    updates

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 ```
 go run main.go --rps 1 --address https://www.somesite.com --headers x-canary:true
 
-go run main.go --rps 1 --address https://www.somesite.com --headers x-canary:true --authentication xxx
+go run main.go --duration 5s --rps 1 --address https://www.somesite.com --headers x-canary:true --authentication xxx
 ```
+
+## to dos
+
+- [ ] Avg of requests duration
+- [ ] log with errors that does not dissapear
--- main.go
+++ main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math"
 	"net/http"
 	"os"
 	"os/exec"
 	"runtime"
+	"slices"
 	"strings"
 	"time"
 
@@ -23,6 +25,7 @@ func main() {
 	authentication := flag.String("authentication", "", "Token required for api authentication")
 	// headers is a list of headers to include in the request
 	headers := flag.StringSlice("headers", []string{}, "Headers to include in the request")
+	duration := flag.Duration("duration", 0, "Duration to run the test for in time format (e.g. 1h30m, 10s, 100ms)")
 	flag.Parse()
 
 	// Validate that required flags are provided
@@ -42,31 +45,114 @@ func main() {
 	lastPrint := time.Now()
 	requests := 0
 	limiter := rate.NewLimiter(rate.Limit(*rps), 1)
-	successChannel := make(chan int)
+
+	successChannel := make(chan time.Duration, 100)
 	successCount := 0
-	errorChannel := make(chan int)
+	successDurations := []time.Duration{}
+
+	errorsChannel := make(chan string, 100)
 	errorCount := 0
+	errors := []string{}
 
 	for {
 		select {
-		case <-successChannel:
+		case duration := <-successChannel:
+			successDurations = append(successDurations, duration)
 			successCount++
-		case <-errorChannel:
+		case errMsg := <-errorsChannel:
 			errorCount++
+			errors = append(errors, errMsg)
 		default:
 			if limiter.Allow() {
-				go doRequest(*address, *headers, *authentication, successChannel, errorChannel)
+				go doRequest(*address, *headers, *authentication, successChannel, errorsChannel)
 				requests++
 			}
+
 			elapsed := time.Since(started)
-			currentRps := float64(requests) / elapsed.Seconds()
-			if time.Since(lastPrint) > 1*time.Second {
-				clearScreen()
-				log.Printf("Requests: %d, Elapsed time (s): %d, RPS: %s, Success: %d, Error: %d", requests, int(elapsed.Seconds()), fmt.Sprintf("%.2f", currentRps), successCount, errorCount)
+			if time.Since(lastPrint) <= 1*time.Second {
+				printMetrics(requests, successCount, errorCount, successDurations, errors, duration, elapsed, false)
+				lastPrint = time.Now()
+			}
+			if *duration > 0 && time.Since(started) > *duration {
+				printMetrics(requests, successCount, errorCount, successDurations, errors, duration, elapsed, true)
+				os.Exit(0)
 			}
 		}
 
 	}
+
+}
+
+func printMetrics(requests int, successCount int, errorCount int, successDurations []time.Duration, errors []string, duration *time.Duration, elapsed time.Duration, printTimes bool) {
+	currentRps := float64(requests) / elapsed.Seconds()
+	// count different errors and show
+	formattedErrors := ""
+	if len(errors) > 0 {
+		errorMap := make(map[string]int)
+		for _, err := range errors {
+			errorMap[err]++
+		}
+		for k, v := range errorMap {
+			formattedErrors += fmt.Sprintf("Error %s: %d\n", k, v)
+		}
+	}
+
+	slices.Sort(successDurations)
+	p99 := percentile(successDurations, 0.99)
+	p95 := percentile(successDurations, 0.95)
+	p90 := percentile(successDurations, 0.90)
+	avg := time.Duration(0)
+	if len(successDurations) > 0 {
+		for _, d := range successDurations {
+			avg += d
+		}
+		avg = avg / time.Duration(len(successDurations))
+	}
+
+	// metrics
+	metrics := map[string]interface{}{
+		"requests":      requests,
+		"elapsed":       elapsed.Truncate(time.Second),
+		"rps":           fmt.Sprintf("%.2f", currentRps),
+		"success count": successCount,
+		"error count":   errorCount,
+		"avg duration":  avg.Round(time.Millisecond),
+	}
+	if *duration > 0 {
+		metrics["duration"] = *duration
+	}
+	var formated string
+
+	keys := []string{}
+	for k := range metrics {
+		keys = append(keys, k)
+	}
+	slices.Sort(keys)
+	for _, key := range keys {
+		value := metrics[key]
+		formated += fmt.Sprintf("%s: %v ", key, value)
+	}
+	clearScreen()
+	log.Print(formattedErrors)
+	log.Print(formated)
+	if printTimes {
+		latency := fmt.Sprintf("p99: %s, p95: %s, p90: %s", p99.Round(time.Millisecond), p95.Round(time.Millisecond), p90.Round(time.Millisecond))
+		log.Print(latency)
+	}
+}
+
+func percentile(durations []time.Duration, p float64) time.Duration {
+	if len(durations) == 0 {
+		return 0
+	}
+	idx := int(math.Ceil(p*float64(len(durations)))) - 1
+	if idx < 0 {
+		idx = 0
+	}
+	if idx >= len(durations) {
+		idx = len(durations) - 1
+	}
+	return durations[idx]
 }
 
 func clearScreen() {
@@ -82,18 +168,19 @@ func clearScreen() {
 	}
 }
 
-func doRequest(url string, headers []string, authentication string, successChannel chan int, errorChannel chan int) {
+func doRequest(url string, headers []string, authentication string, successChannel chan time.Duration, errorsChannel chan string) {
 	client := &http.Client{}
+	start := time.Now()
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Printf("Error creating request: %v", err)
-		// count error
-		errorChannel <- 1
+		errorsChannel <- "Error creating request"
+		return
 	}
 	for _, header := range headers {
 		headerParts := strings.Split(header, ":")
 		if len(headerParts) != 2 {
 			log.Printf("Invalid header: %s", header)
+			os.Exit(1)
 			continue
 		}
 		req.Header.Set(headerParts[0], headerParts[1])
@@ -104,16 +191,15 @@ func doRequest(url string, headers []string, authentication string, successChann
 	resp, err := client.Do(req)
 	io.Copy(io.Discard, resp.Body)
 	if err != nil {
-		log.Printf("Error making request: %v", err)
-		// count error
-		errorChannel <- 1
+		errorsChannel <- "Error making request"
+		return
 	}
+	elapsed := time.Since(start)
 	if resp.StatusCode != 200 {
 		log.Printf("Error: Status code %d", resp.StatusCode)
-		// count error
-		errorChannel <- 1
+		errorsChannel <- fmt.Sprintf("%d", resp.StatusCode)
+		return
 	}
-	// count success
-	successChannel <- 1
+	successChannel <- elapsed
 	defer resp.Body.Close()
 }
